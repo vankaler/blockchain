ef0e14da2469fff0b75a25a06c14bbf44bac0f42
Nejc <vankaler@proton.me>
2023-12-29 17:02:26 +0100

    Add project files.

---
+++ Blockchain/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace Blockchain
+{
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            App app = new App();
+            MainWindow mainWindow = new MainWindow();
+            app.Run(mainWindow);
+        }
+    }
+}
